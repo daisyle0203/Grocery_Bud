2739faba6c9e70889aa58a8d34089e99a2462f3c
Daisy Le <daisy.le0203@gmail.com>
2022-10-30 16:40:45 -0500

    added comments

--- src/Alert.js
+++ src/Alert.js
@@ -1,12 +1,13 @@
 import React, { useEffect } from "react"
 
-const Alert = ({ type, msg, removeAlert }) => {
+const Alert = ({ type, msg, removeAlert, list }) => {
   useEffect(() => {
     const timeout = setTimeout(() => {
       removeAlert()
     }, 3000)
+    // Once something changes from the list, clear out the timeout
     return () => clearTimeout(timeout)
-  }, [])
+  }, [list]) // Every time the list change, get a new timer
   return <p className={`alert alert-${type}`}>{msg}</p>
 }
 
