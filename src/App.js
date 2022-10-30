ef02a9d7d36a1529ce60353f7644a13acf1bcb1d
Daisy Le <daisy.le0203@gmail.com>
2022-10-30 17:03:00 -0500

    added local storage

--- src/App.js
+++ src/App.js
@@ -2,9 +2,21 @@ import React, { useState, useEffect } from "react"
 import List from "./List"
 import Alert from "./Alert"
 
+const getLocalStorage = () => {
+  // Get the item from local storage by key
+  let list = localStorage.getItem("list")
+  // If the list exists, return the list
+  if (list) {
+    return JSON.parse(localStorage.getItem("list"))
+    // If not, return an empty array
+  } else {
+    return []
+  }
+}
+
 function App() {
   const [name, setName] = useState("")
-  const [list, setList] = useState([])
+  const [list, setList] = useState(getLocalStorage())
   const [isEditing, setIsEditing] = useState(false)
   const [editID, setEditID] = useState(null)
   const [alert, setAlert] = useState({
@@ -64,6 +76,11 @@ function App() {
     setEditID(id)
     setName(specificItem.title)
   }
+
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list))
+  }, [list])
+
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
