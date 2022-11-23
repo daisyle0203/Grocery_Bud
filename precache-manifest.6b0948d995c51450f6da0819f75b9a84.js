34465df0af874acb2d00bf0333ea84778c53cdca
Daisy Le <daisy.le0203@gmail.com>
2022-11-23 01:33:26 -0600

    Updates

---
+++ precache-manifest.6b0948d995c51450f6da0819f75b9a84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bd43612e600e21e0ddbb44322759109",
+    "url": "/Grocery_Bud/index.html"
+  },
+  {
+    "revision": "6de9ddae6d3d1dbb3106",
+    "url": "/Grocery_Bud/static/css/main.65fc46b8.chunk.css"
+  },
+  {
+    "revision": "a35ae579dce300ee7c7c",
+    "url": "/Grocery_Bud/static/js/2.df29e4cf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Grocery_Bud/static/js/2.df29e4cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6de9ddae6d3d1dbb3106",
+    "url": "/Grocery_Bud/static/js/main.deb39710.chunk.js"
+  },
+  {
+    "revision": "a8d8ca9dc00588975453",
+    "url": "/Grocery_Bud/static/js/runtime-main.e8878a61.js"
+  }
+]);
